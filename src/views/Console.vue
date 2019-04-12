8358a4e65016c6083ff4aba82b59e7a5eb95d51b
zhibi <1017866168@qq.com>
2019-04-12 10:38:29 +0800

    complete console-nav

--- src/views/Console.vue
+++ src/views/Console.vue
@@ -1,5 +1,15 @@
 <template>
-
+  <div class="console">
+    <nav class="console-nav">
+      <div class="console-nav-logo">
+        <img src="../assets/英文LOGO白色.png"/>
+      </div>
+      <div class="console-nav-title">东诚数字看板后台</div>
+      <div class="console-nav-date">
+        {{nowDate}}
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -7,6 +17,7 @@ export default {
   name: 'Console',
   data () {
     return {
+      nowDate: this.dayjs.format('YYYY-MM-DD')
     }
   }
 }
