a9aaf7a64a1a020c4de2e8424c450d48a777f7fa
zhibi <1017866168@qq.com>
2019-04-12 13:41:23 +0800

    translate to chinese

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
+    <a-locale-provider :locale="zhCN">
+      <router-view/>
+    </a-locale-provider>
   </div>
 </template>
+
+<script>
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+export default {
+  data () {
+    return {
+      zhCN
+    }
+  }
+}
+</script>
 
 <style lang="less">
 #app {
