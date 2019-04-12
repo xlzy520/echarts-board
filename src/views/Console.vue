93318bf3e3481b6f9c883afcb50a7cbff561a246
zhibi <1017866168@qq.com>
2019-04-12 16:47:11 +0800

    add team-select component

--- src/views/Console.vue
+++ src/views/Console.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="console">
     <console-nav></console-nav>
-
+    <team-select></team-select>
   </div>
 </template>
 
 <script>
 import ConsoleNav from '../components/ConsoleNav'
+import TeamSelect from '../components/TeamSelect'
 export default {
   name: 'Console',
   components: {
-    ConsoleNav
+    ConsoleNav,
+    TeamSelect
   },
   data () {
     return {}
@@ -22,6 +24,7 @@ export default {
   .console {
     position: relative;
     display: flex;
+    flex-direction: column;
     min-width: 1300px;
     min-height: 600px;
     height: 100%;
