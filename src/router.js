f39c6ab969163c1d5a62b112044f9c7f913c8b1e
zhibi <1017866168@qq.com>
2019-04-12 09:50:56 +0800

    init pages

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Console from './views/Console.vue'
 
 Vue.use(Router)
 
@@ -8,16 +8,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'console',
+      component: Console
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/dashboard',
+      name: 'dashboard',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/Dashboard.vue')
     }
   ]
 })
---
+++ src/views/Console.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'Console',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
