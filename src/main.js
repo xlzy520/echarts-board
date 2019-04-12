5f15a53aea63f24bf763fed5ab40c485af0ff7a8
zhibi <1017866168@qq.com>
2019-04-12 10:35:15 +0800

    add dayjs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dayjs from 'dayjs'
+
+import './styles/index.less'
+
+Vue.prototype.dayjs = dayjs()
+
 
 Vue.config.productionTip = false
 
