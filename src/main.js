eb9490089ec7f56730dffe98f48947d2de4f9629
zhibi <1017866168@qq.com>
2019-04-15 09:22:38 +0800

    add tabs component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Checkbox, DatePicker, LocaleProvider } from 'ant-design-vue'
+import { Checkbox, DatePicker, LocaleProvider, Tabs } from 'ant-design-vue'
 
 import moment from 'moment'
 
@@ -13,6 +13,7 @@ Vue.prototype.moment = moment
 Vue.use(Checkbox)
 Vue.use(DatePicker)
 Vue.use(LocaleProvider)
+Vue.use(Tabs)
 
 Vue.config.productionTip = false
 
