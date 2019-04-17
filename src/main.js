d3c456969bef9c048d8c038e38f60520c021e09e
zhibi <1017866168@qq.com>
2019-04-17 10:28:09 +0800

    增加Select, Radio, Spin, message，配置message

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Checkbox, DatePicker, LocaleProvider, Tabs, Table, Button, Modal } from 'ant-design-vue'
+import { Checkbox, DatePicker, LocaleProvider, Tabs, Table, Button, Select, Radio, Spin, message } from 'ant-design-vue'
 
 import moment from 'moment'
 
 import './styles/index.less'
+import './styles/ant-message.less'
 
 Vue.prototype.moment = moment
+Vue.prototype.$message = message
 
 Vue.use(Checkbox)
 Vue.use(DatePicker)
@@ -16,7 +18,15 @@ Vue.use(LocaleProvider)
 Vue.use(Tabs)
 Vue.use(Table)
 Vue.use(Button)
-Vue.use(Modal)
+Vue.use(Select)
+Vue.use(Radio)
+Vue.use(Spin)
+
+message.config({
+  top: `80px`,
+  duration: 1.5,
+  maxCount: 3
+})
 
 Vue.config.productionTip = false
 
