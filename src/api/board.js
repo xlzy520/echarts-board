776098351dd59369d3435659a8a3402d12e82795
zhibi <1017866168@qq.com>
2019-05-05 10:35:32 +0800

    默认为get请求，增加后面三个接口

--- src/api/board.js
+++ src/api/board.js
@@ -6,29 +6,49 @@ export default {
    * 获取平均车速看板数据
     */
   getSpeed () {
-    return request.get(`${baseUrl}/getSpeed`)
+    return request(`${baseUrl}/getSpeed`)
   },
   /**
    * 获取停机次数看板数据
-   * @param data
    * @returns {*}
    */
   getHaltCount () {
-    return request.get(`${baseUrl}/getHaltCount`)
+    return request(`${baseUrl}/getHaltCount`)
   },
   /**
    * 获取客诉率看板数据
    * @returns {*}
    */
   getCustomComplaintRate () {
-    return request.get(`${baseUrl}/getCustomComplaintRate`)
+    return request(`${baseUrl}/getCustomComplaintRate`)
   },
   /**
    * 获取原纸利用率数据
    * @returns {*}
    */
   getPaperUseRate () {
-    return request.get(`${baseUrl}/getPaperUseRate`)
+    return request(`${baseUrl}/getPaperUseRate`)
+  },
+  /**
+   * 获取平均门幅和修边比率看板数据
+   * @returns {*}
+   */
+  getTrimmingRateAndAvgWidth () {
+    return request(`${baseUrl}/getTrimmingRateAndAvgWidth`)
+  },
+  /**
+   * 获取欠数率和准时入库率看板数据
+   * @returns {*}
+   */
+  getOwnRateAndPunctualRate () {
+    return request(`${baseUrl}/getOwnRateAndPunctualRate`)
+  },
+  /**
+   * 获取制程损耗和维修费用看板数据
+   * @returns {*}
+   */
+  getProcessLossAndCost () {
+    return request(`${baseUrl}/getProcessLossAndCost`)
   }
 
 }
