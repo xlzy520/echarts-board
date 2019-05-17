6abf63338282008f78d22077640a9e3de7a9eefd
zhibi <1017866168@qq.com>
2019-05-17 17:04:19 +0800

    完成客诉率堆叠图

--- src/views/KeSuLv.vue
+++ src/views/KeSuLv.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="charts-wrapper">
-    <chart-box ref="top" id="top" chartStyle="height: 272px"></chart-box>
+    <chart-box ref="top" id="top" chartStyle="height: 292px"></chart-box>
     <chart-box
       ref="middle"
       title="总线数据"
@@ -14,7 +14,7 @@
       :header="chartHeader"
       :group-data="data.teamTrendMap"
       @tabClick="tabClick"
-      chartStyle="height: 290px"
+      chartStyle="height: 270px"
     ></chart-box>
   </div>
 </template>
@@ -39,80 +39,121 @@ export default {
   methods: {
     // 客诉率
     drawKeSuLv () {
+      this.data.avgComplaintRate = this.data.avgComplaintRate.reverse()
+      const [a, b] = this.data.avgComplaintRate
+      const max = Math.max(a.complaintRateMonthGoalRate, b.complaintRateMonthGoalRate)
+      const markPointData = [
+        {
+          name: '日累计',
+          value: b.complaintRate,
+          xAxis: b.complaintRateMonthRate >= a.complaintRateMonthGoalRate ? max : b.complaintRateMonthRate * (max / b.complaintRateMonthGoalRate),
+          yAxis: 1
+        },
+        {
+          name: '日累计',
+          value: a.complaintRate,
+          xAxis: a.complaintRateMonthRate >= a.complaintRateMonthGoalRate ? max : a.complaintRateMonthRate * (max / a.complaintRateMonthGoalRate),
+          yAxis: 0
+        }
+      ]
+      // 提示框标记坐标
+      let markPointRectData = JSON.parse(JSON.stringify(markPointData))
+      // 提示框左侧和右侧都不能被隐藏
+      markPointRectData.map(v => {
+        if ((v.xAxis - v.value) >= max) {
+          v.xAxis -= v.value * 2
+        }
+        let rate = v.xAxis / max
+        if (rate < 0.1) {
+          v.xAxis += max * 0.13
+        } else if (rate > 0.9) {
+          v.xAxis -= max * 0.13
+        }
+      })
+      // 标记线数据，上下两列
+      let markLineData = [
+        // 下标记线
+        [
+          {
+            x: '15',
+            y: '77.5%',
+            lineStyle: { width: 40, color: '#ff001c' }
+          },
+          { x: 525, y: '77.5%' }
+        ],
+        // 上标记线
+        [
+          {
+            x: '15',
+            y: '39.3%',
+            lineStyle: { width: 40, color: '#ff001c' }
+          },
+          { x: '525', y: '39.3%' }
+        ]
+      ]
+      // 标记线数量，长度
+      let monthCount = this.data.avgComplaintRate.map((v, index) => {
+        // 长度是否为2，如果长度不为2，那么下标只能取0，而取不到1
+        const lengthIs2 = markLineData.length === 2
+        if (v.complaintRateMonthRate >= v.complaintRateMonthGoalRate) {
+          return max
+        } else {
+          // 未超过，不显示标记线
+          markLineData.splice(lengthIs2 ? index : 0, 1)
+        }
+        return v.complaintRateMonthRate * (max / v.complaintRateMonthGoalRate)
+      })
       let option = {
-        color: ['#000f84', '#5095f3'],
+        color: ['#198cff', '#d0e7ff'],
         title: {
           text: '客诉率',
           textStyle: {
-            fontFamily: 'PingFang SC Regular',
-            fontSize: 120 / 4,
-            fontWeight: 'normal'
+            fontFamily: 'PingFang SC Bold',
+            fontSize: 30,
+            fontWeight: 'lighter'
           },
-          left: 48 / 4,
-          top: 40 / 4
+          left: 12,
+          top: 15
         },
         legend: {
-          data: [
-            {
-              name: '月累计完成',
-              icon: 'rect',
-              textStyle: {
-                padding: [0, 0, 0, 28 / 4]
-              }
-            },
-            {
-              name: '月目标',
-              icon: 'rect',
-              textStyle: {
-                padding: [0, 0, 0, 28 / 4]
-              }
-            }
-          ],
-          right: 120 / 4,
+          textStyle: {
+            fontFamily: 'PingFang SC Regular',
+            fontSize: 100 / 4
+          },
+          right: 15,
           top: 66 / 4,
           itemHeight: 96 / 4,
           itemWidth: 96 / 4,
           itemGap: 76 / 4,
-          textStyle: {
-            fontFamily: 'PingFang SC Regular',
-            fontSize: 100 / 4,
-            itemGap: 10 / 4
-          }
+          data: [
+            {
+              name: '月累计',
+              icon: 'rect'
+            },
+            {
+              name: '月目标',
+              icon: 'rect'
+            }
+          ]
         },
         grid: {
-          top: 320 / 4,
-          left: -100 / 4,
-          bottom: -30,
-          right: 560 / 4,
+          top: 60,
+          left: 15,
+          right: 15,
+          bottom: -10,
           containLabel: true
         },
         xAxis: {
           show: false,
-          type: 'value'
+          type: 'value',
+          max: max
         },
         yAxis: {
           type: 'category',
-          inverse: true,
-          offset: -20,
-          data: this.data.avgComplaintRate.map(v => v.name),
           axisLabel: {
-            color: '#333',
-            fontFamily: 'PingFang SC Regular',
-            fontSize: 120 / 4,
-            margin: 140 / 4,
-            formatter: (params, index) => {
-              return '{main|' + params + '}' + '\n{sub|' + this.data.avgComplaintRate[index].complaintRate + '%}'
-            },
-            rich: {
-              main: {
-                align: 'left',
-                fontSize: 120 / 4
-              },
-              sub: {
-                fontSize: 80 / 4
-              }
-            }
+            show: false
           },
+          data: this.data.avgComplaintRate.map(v => v.name),
           splitLine: {
             show: false
           },
@@ -125,43 +166,152 @@ export default {
         },
         series: [
           {
-            name: '月累计完成',
+            name: '月累计',
             type: 'bar',
             barWidth: 160 / 4,
-            barGap: '0%',
+            stack: 'count',
             label: {
-              normal: {
-                show: true,
-                position: 'right',
-                fontFamily: 'PingFang SC Regular',
-                fontSize: 120 / 4,
-                color: '#333',
-                formatter: (paramas, index) => {
-                  return paramas.data + '%'
+              show: true,
+              position: 'insideLeft',
+              fontFamily: 'PingFang SC Regular',
+              fontSize: 18,
+              offset: [0, -40],
+              color: '#333',
+              formatter: params => {
+                return '{letter|' + params.name + '} 组今日客诉率 ' +
+                  '{number|' + (params.dataIndex === 0 ? a : b).complaintRate + '}%'
+              },
+              rich: {
+                letter: {
+                  fontFamily: 'PingFang SC Regular',
+                  color: '#333',
+                  fontSize: 26
                 },
-                offset: [44 / 4, 0]
+                number: {
+                  fontFamily: 'PingFang SC Regular',
+                  color: '#333',
+                  fontSize: 30
+                }
               }
             },
-            data: this.data.avgComplaintRate.map(v => v.complaintRateMonthRate)
+            markPoint: {
+              symbol: 'arrow',
+              symbolSize: 15,
+              symbolOffset: [0, 20],
+              itemStyle: {
+                color: '#198cff'
+              },
+              data: markPointData,
+              label: {
+                show: false
+              }
+            },
+            markLine: {
+              symbol: 'none',
+              lineStyle: { type: 'solid' },
+              silent: true,
+              label: {
+                position: 'middle',
+                color: '#fff',
+                fontFamily: 'PingFang SC Regular',
+                fontSize: 18,
+                verticalAlign: 'middle',
+                formatter: params => {
+                  const { complaintRateMonthGoalRate, complaintRateMonthRate } = params.dataIndex === 0 ? b : a
+                  const value = complaintRateMonthRate - complaintRateMonthGoalRate
+                  if (value === 0) {
+                    return '已触及月目标'
+                  } else if (value > 0) {
+                    return '已超出月目标{number|' + value.toFixed(3) + '}%'
+                  }
+                  return ''
+                },
+                rich: {
+                  number: {
+                    fontFamily: 'PingFang SC Regular',
+                    fontSize: 30,
+                    color: '#fff'
+                  }
+                }
+              },
+              data: markLineData
+            },
+            data: monthCount
           },
+          // {
+          //   name: '日累计',
+          //   type: 'bar',
+          //   barWidth: 160 / 4,
+          //   barGap: '0%',
+          //   stack: 'count',
+          //   label: {
+          //     normal: {
+          //       show: false,
+          //       position: 'top',
+          //       fontFamily: 'PingFang SC Regular',
+          //       fontSize: 120 / 4,
+          //       color: '#333'
+          //     }
+          //   },
+          //   data: this.data.avgComplaintRate.map(v => {
+          //     return v.complaintRate * (max / v.complaintRateMonthGoalRate)
+          //   }),
+          //   markPoint: {
+          //     symbol: 'arrow',
+          //     symbolSize: 15,
+          //     symbolOffset: [0, 20],
+          //     data: markPointData,
+          //     label: {
+          //       show: false
+          //     }
+          //   }
+          // },
           {
             name: '月目标',
             type: 'bar',
             barWidth: 160 / 4,
+            stack: 'count',
             label: {
-              normal: {
-                show: true,
-                position: 'right',
-                fontFamily: 'PingFang SC Regular',
-                fontSize: 120 / 4,
-                color: '#333',
-                formatter: (paramas, index) => {
-                  return paramas.data + '%'
-                },
-                offset: [44 / 4, 0]
+              show: true,
+              position: 'insideRight',
+              offset: [0, -40],
+              fontFamily: 'PingFang SC Regular',
+              fontSize: 18,
+              color: '#333',
+              formatter: params => {
+                return '月目标客诉率{number|' + (params.dataIndex === 0 ? a : b).complaintRateMonthGoalRate + '}%'
+              },
+              rich: {
+                number: {
+                  fontFamily: 'PingFang SC Regular',
+                  color: '#333',
+                  fontSize: 30
+                }
               }
             },
-            data: this.data.avgComplaintRate.map(v => v.complaintRateMonthGoalRate)
+            data: this.data.avgComplaintRate.map(v => {
+              if (v.complaintRateMonthRate >= v.complaintRateMonthGoalRate) {
+                return 0
+              }
+              return max - v.complaintRateMonthRate * max / v.complaintRateMonthGoalRate
+            }),
+            markPoint: {
+              symbol: 'rect',
+              symbolSize: [150, 30],
+              symbolOffset: [0, 45],
+              data: markPointRectData,
+              itemStyle: {
+                color: '#198cff'
+              },
+              label: {
+                fontFamily: 'PingFang SC Regular',
+                fontSize: 15,
+                formatter: params => {
+                  return '月累计客诉率 ' + (params.dataIndex === 0 ? b : a).complaintRateMonthRate + '%'
+                },
+                offset: [0, 0]
+              }
+            }
           }
         ]
       }
@@ -267,7 +417,7 @@ export default {
             show: false
           },
           axisLabel: {
-           show: false
+            show: false
           },
           axisLine: {
             show: false
@@ -276,6 +426,7 @@ export default {
         yAxis: {
           type: 'value',
           min: 0,
+          max: 0.15,
           splitNumber: 4,
           interval: 0.05,
           axisLine: {
